7f6a0b4a3f441b0a93a3eb3d6887a476f36bb27e
developiaa <developiaa@gmail.com>
2024-03-10 18:04:14 +0900

    first commit

---
+++ basic/src/test/java/pro/developia/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.developia.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
