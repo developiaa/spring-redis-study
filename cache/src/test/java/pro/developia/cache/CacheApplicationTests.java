0906373ece238937558f8d8fb6769952b288acff
developiaa <developiaa@gmail.com>
2024-03-17 10:30:00 +0900

    feat: cache 모듈 추가

---
+++ cache/src/test/java/pro/developia/cache/CacheApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.developia.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
