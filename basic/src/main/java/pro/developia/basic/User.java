55e8581a58df6de6bd4b4d12e0750bee222bfbf6
developiaa <developiaa@gmail.com>
2024-03-17 10:00:00 +0900

    feat: cache 실습

---
+++ basic/src/main/java/pro/developia/basic/User.java
@@ -0,0 +1,35 @@
+package pro.developia.basic;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 30)
+    private String name;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ basic/src/main/java/pro/developia/basic/UserRepository.java
@@ -0,0 +1,6 @@
+package pro.developia.basic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
